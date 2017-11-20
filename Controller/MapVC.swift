a2502f9c0c47b3844bd17c4c4a29ad724c241d60
Sahariar Kabir <sronok@gmail.com>
2017-11-21 05:49:21 +0600

    Initial commit

---
+++ Controller/MapVC.swift
@@ -0,0 +1,25 @@
+//
+//  MapVc.swift
+//  map-city
+//
+//  Created by Sahariar Kabir on 21/11/17.
+//  Copyright © 2017 Sahariar Kabir. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
